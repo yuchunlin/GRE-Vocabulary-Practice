50e58ba8082064de9456ac023c2ee6cf8c082acd
Yu-Chun Lin <yu.chun.lin@berkeley.edu>
2017-05-21 08:14:10 -0500

    All basic functionalities working

--- grepractice.py
+++ grepractice.py
@@ -19,16 +19,20 @@ def generatequestion():
      	definition = word_definition_pairs[rand_option][1]
      	possible_answers = []
      	possible_answers.append(definition)
+     	correct_choice = 0
      	for i in range(0, 4):
      		elem = word_definition_pairs[random.randint(0, len(word_definition_pairs) - 1)][1]
      		while elem in possible_answers:
      			elem = word_definition_pairs[random.randint(0, len(word_definition_pairs) - 1)][1]
      		possible_answers.append(elem)
      	print("The word is:\t" + word)
-     	for j in range(1, len(possible_answers)): 
+     	for j in range(1, len(possible_answers) + 1): 
      		choice = random.choice(possible_answers)
+     		if choice == definition:
+     			correct_choice = j
      		possible_answers.remove(choice)
-     		print("" + j + ":\t" + choice)
+     		print("" + str(j) + ":\t" + choice)
+     	return (correct_choice, definition)
 
 
 
@@ -42,17 +46,25 @@ def GREPractice():
         mode = raw_input("Please enter the desired mode: \t")
         mode = str(mode)
         if mode   == '1':
-            print("Entering practice mode.")
-            print("Please select the correct definition, or type exit to return to the main menu.")
+            print("\nEntering practice mode.\n")
+            print("Please select the correct definition, or type exit to return to the main menu.\n")
             continue_test = True
             while continue_test:
-            	generatequestion()
+            	(w, d) = generatequestion()
+            	w = str(w)
+            	d = str(d)
+            	d = d.split(" ", 1)[1]
             	answer = raw_input("Your answer: \t Definition ")
             	if answer.lower() == "exit":
             		continue_test = False
+            	elif answer.lower() == w:
+            		print("Correct!\n")
+            	else:
+            		quotes = '"'
+            		print("Incorrect. The correct answer is " + quotes + d + quotes + ".\n")
 
         elif mode == '2':
-            print("Entering edit mode.")
+            print("\nEntering edit mode. \n")
             edit_requested = True
             while edit_requested:
                 editmode()
